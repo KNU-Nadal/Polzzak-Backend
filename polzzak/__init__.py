b55c7e16dce1b334f0ec73b6c75ec1abbfa276a1
YoonGangHoon <yghun021007@naver.com>
2024-09-16 16:46:49 +0900

    Feat: api 생성 후 swagger 연동

---
+++ polzzak/__init__.py
@@ -0,0 +1,23 @@
+from flask import Flask
+from flask_restx import Api
+from flask_migrate import Migrate
+
+import config
+
+from .database import db
+from . import models
+
+app = Flask(__name__)
+app.config.from_object(config)
+
+api = Api(
+    app,
+    version='0.1',
+    title="Nadal's API Server",
+    description="Nadal's Polzzak_Backend API Server!",
+    contact="yghun021007@knu.ac.kr",
+)
+
+db.init_app(app)
+migrate = Migrate()
+migrate.init_app(app,db)
