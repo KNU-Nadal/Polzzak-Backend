93f1b5621a86c82dd089cf6a0721fcdeda95cf59
YoonGangHoon <yghun021007@naver.com>
2024-09-18 02:13:29 +0900

    Chore: Place, Review, Team 모델 수정 및 Event 모델 추가

--- polzzak/models.py
+++ polzzak/models.py
@@ -5,6 +5,12 @@ user_team = db.Table(
     db.Column('team_id', db.Integer, db.ForeignKey('team.id', ondelete='CASCADE'), primary_key = True)
 ) #user와 team을 연결하는 테이블
 
+user_event = db.Table(
+    'user_event',
+    db.Column('user_id', db.Integer, db.ForeignKey('user.id', ondelete='CASCADE'), primary_key = True),
+    db.Column('event_id', db.Integer, db.ForeignKey('event.id', ondelete='CASCADE'), primary_key = True)
+) #user와 event를 연결하는 테이블
+
 class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     kakao_id = db.Column(db.String(80), unique=True, nullable=False)
@@ -16,14 +22,6 @@ class User(db.Model):
         self.name = name
         self.profile_image = profile_image
 
-class Team(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    title = db.Column(db.String(100), unique=True, nullable=False)
-    content = db.Column(db.Text(), nullable=False)
-    start_time = db.Column(db.DateTime(), nullable=False)
-    end_time = db.Column(db.DateTime(), nullable=False)
-    admin_id = db.Column(db.Integer, db.ForeignKey('user.id', ondelete='CASCADE'), nullable=False)
-
 class Review(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     title = db.Column(db.String(100), nullable=False)
@@ -34,10 +32,24 @@ class Review(db.Model):
 class Place(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(150), unique=True, nullable=False)
-    detail = db.Column(db.String(150), unique=True, nullable=False)
+    latitude = db.Column(db.Float, nullable=False)
+    longitude = db.Column(db.Float, nullable=False)
 
-class Festival(db.Model):
+class Team(db.Model):
     id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(100), unique=True, nullable=False)
+    content = db.Column(db.Text(), nullable=False)
+    start_time = db.Column(db.DateTime(), nullable=False)
+    end_time = db.Column(db.DateTime(), nullable=False)
+    admin_id = db.Column(db.Integer, db.ForeignKey('user.id', ondelete='CASCADE'), nullable=False)
+    place_id = db.Column(db.Integer, db.ForeignKey('place.id', ondelete='CASCADE'), nullable=False)
+
+class Event(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(100), unique=True, nullable=False)
+    content = db.Column(db.Text(), nullable=False)
+    start_time = db.Column(db.DateTime(), nullable=False)
+    end_time = db.Column(db.DateTime(), nullable=False)
     place_id = db.Column(db.Integer, db.ForeignKey('place.id', ondelete='CASCADE'), nullable=False)
 
 class Market(db.Model):
