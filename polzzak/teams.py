7ccc3618489b6a8b39ea77306354ffcfed0c4d0d
YoonGangHoon <yghun021007@naver.com>
2024-09-19 17:41:50 +0900

    Feat: Team 객체를 return 할 때 datetime을 return 하는 기능 추가, 현재 로그인 되어 있는 유저가 팀에 속해 있는 유저인지  판단하는 기능 추가, Team 객체가 생성되었을 때 팀 id를 return 하는 기능 추가, SQL 쿼리문으롤 작성 되었던 부분 ORM으로 수정, admin이 팀에서 나가려 할 시, 팀을 삭제하는 기능 추가

--- polzzak/teams.py
+++ polzzak/teams.py
@@ -1,21 +1,20 @@
 from flask import Flask, request, session
 from flask_restx import Resource, fields, reqparse, Namespace
 from . import db
-from .models import Team, user_team
+from .models import Team, User, user_team
 from sqlalchemy import select, insert, delete
 from datetime import datetime
 
 Team_ns = Namespace(name="team",description="플로깅 팀을 위한 API")
 
 teamForm = reqparse.RequestParser()
-teamForm.add_argument('user_id', type=int, default=None, help='user id')
 teamForm.add_argument('id', type=int, default=None, help='team id')
 
 team_create_fields = Team_ns.model('team_create', {
     'title': fields.String,
     'content': fields.String,
-    'start_time': fields.String,
-    'end_time': fields.String,
+    'start_time': fields.String(default = '2024-09-24T14:30'),
+    'end_time': fields.String(default = '2024-09-28T14:30'),
     'place_id': fields.Integer
 }) #리뷰 생성 필드
 
@@ -23,8 +22,8 @@ team_modify_fields = Team_ns.model('team_modify', {
     'id' : fields.Integer,
     'title': fields.String,
     'content': fields.String,
-    'start_time': fields.String,
-    'end_time': fields.String,
+    'start_time': fields.String(default = '2024-09-25T14:30'),
+    'end_time': fields.String(default = '2024-09-29T14:30'),
     'place_id' : fields.Integer
 }) #리뷰 수정 필드
 
@@ -36,38 +35,42 @@ team_delete_fields = Team_ns.model('team_delete', {
 class Teams(Resource):
     @Team_ns.expect(teamForm)
     def get(self):
-#1.이 유저가 이미 이 팀에 가입되어있는지 확인을 하고, 가입 되었으면, return  message this user 이미 그거
+        #1.이 유저가 이미 이 팀에 가입되어있는지 확인을 하고, 가입 되었으면, return  message this user 이미 그거
         user_id = session.get('user_id')
         id = request.args.get('id')
 
+        user = User.query.get_or_404(user_id)
         team = Team.query.get_or_404(id)
-        user_team_entry = db.session.execute(
-            select([user_team]).where(user_team.c.user_id == user_id, user_team.c.team_id == id)
-        ).first()
 
-        if user_team_entry:
-            return {'message': 'You are already a member of this team',
+        start_time = team.start_time
+        end_time = team.end_time
+        # datetime 객체를 문자열로 변환
+        start_time_str = start_time.strftime('%Y-%m-%d %H:%M:%S')
+        end_time_str = end_time.strftime('%Y-%m-%d %H:%M:%S')
+        # 유저가 팀에 속해 있는지 확인
+        if user and team:
+            if team in user.user_team_set:
+                return {'message': 'You are already a member of this team',
                     'team' : {
                     'id' : team.id,
                     'title' : team.title,
                     'content' : team.content,
-                    'start_time' : team.start_time,
-                    'end_time' : team.end_time,
+                    'start_time' : start_time_str,
+                    'end_time' : end_time_str,
                     'place_id' : team.place_id
                 }}, 200
-        else:
-            return {
+            else:
+                return {
                     'message' : 'can join this team',
                     'team' : {
                         'id' : team.id,
                         'title' : team.title,
                         'content' : team.content,
-                        'start_time' : team.start_time,
-                        'end_time' : team.end_time,
+                        'start_time' : start_time_str,
+                        'end_time' : end_time_str,
                         'place_id' : team.place_id
                     }
             }, 200
-
 
     @Team_ns.expect(team_create_fields)
     def post(self):
@@ -82,15 +85,16 @@ class Teams(Resource):
         end_time = datetime.fromisoformat(end_time_str)
         new_team = Team(title=title, content=content, start_time=start_time, end_time=end_time,admin_id=admin_id, place_id=place_id)
 
-        id = db.session.flush()
-
         db.session.add(new_team)
-        db.session.commit()
 
+        user = User.query.filter_by(id=admin_id).first()
+        team = Team.query.filter_by(id=new_team.id).first()
+        user.user_team_set.append(team)
+        db.session.commit()
 
         return {'message': 'Team created successfully!',
                 'team':{
-                    'id': id
+                    'id': new_team.id
                 }}, 201
     
     @Team_ns.expect(team_modify_fields)
@@ -121,7 +125,7 @@ class Teams(Resource):
     @Team_ns.expect(team_delete_fields)
     def delete(self):
         id = request.json.get('id')
-        team = Team.query.get_or_404(id)
+        team = Team.query.get(id)
         db.session.delete(team)
         db.session.commit()
 
@@ -145,11 +149,17 @@ class Join(Resource):
         user_id = session.get('user_id')
         if user_id == 0:
             return {'message' : 'login plz'}
-        # user_team 테이블에 데이터 삽입
-        new_user_team = insert(user_team).values(user_id=user_id, team_id=team_id)
-        db.session.execute(new_user_team)
-        db.session.commit()
-        return {'message': 'User added to team successfully'}, 201
+        
+        user = User.query.filter_by(id=user_id).first()
+        team = Team.query.filter_by(id=team_id).first()
+
+        # 유저가 존재하고 팀이 존재할 경우에만 처리
+        if user and team:
+            # 유저가 해당 팀에 속하지 않은 경우에만 추가
+            if team not in user.user_team_set:
+                user.user_team_set.append(team)
+                db.session.commit()
+                return {'message': 'User added to team successfully'}, 201
     
     # team_id와 user_id를 조건으로 데이터 삭제
     @Team_ns.expect(userteam_delete_fields)
@@ -158,9 +168,17 @@ class Join(Resource):
         user_id = session.get('user_id')
 
         # user_team 테이블에서 해당 데이터 삭제
-        delete_user_team = delete(user_team).where(user_team.c.user_id == user_id, user_team.c.team_id == team_id)
-        db.session.execute(delete_user_team)
-        db.session.commit()
+        user = User.query.get_or_404(user_id)
+        team = Team.query.get_or_404(team_id)
 
-        return {'message': 'User removed from team successfully'}, 200
-
+        if user_id == team.admin_id:
+            team = Team.query.get(team.id)
+            db.session.delete(team)
+            db.session.commit()
+            return {'message' : 'Team deleted successfully'}, 200
+        # 유저가 팀에 속해 있는지 확인한 후 삭제
+        if user and team:
+            if team in user.user_team_set:
+                user.user_team_set.remove(team)  # 관계 제거
+                db.session.commit()  # 변경 사항 커밋
+                return {'message': 'User removed from team successfully'}, 200
